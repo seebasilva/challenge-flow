4a967c4180559777cb6c2d6fa0dd6c36ae5df0f2
Silva Sebastian Marcelo <sebas.lpb@gmail.com>
2022-08-09 17:05:48 -0300

    Se agregan los componentes principales

---
+++ src/components/currentClimate/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+
+const CurrentClimate = () => {
+    return (
+        <Box>
+            <Card variant="outlined">
+                Clima actual
+            </Card>
+        </Box>
+    )
+}
+
+export default CurrentClimate;
